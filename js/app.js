8b3bd69b56a82aa5382288ce4f10fff8e951a4b3
Diana Fisher <diana.fisher@gmail.com>
2017-08-31 19:31:03 -0700

    Add additional player image

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  this.sprite = 'images/char-boy.png';
+  this.sprite = 'images/char-cat-girl.png';
   this.x = 200;
   this.y = 320;
 };
