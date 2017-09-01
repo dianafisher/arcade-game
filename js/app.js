7e00fefeacf257464cb6232609e8ce49ce7ba84a
Diana Fisher <diana.fisher@gmail.com>
2017-08-31 18:09:32 -0700

    Add player and implement render and update methods

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 200;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,49 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = 200;
+  this.y = 320;
+};
 
+Player.prototype.handleInput = function(key) {
+  console.log('handle player input', key);
+  var x, y;
+  switch(key) {
+    case 'left':
+      x = Math.max(this.x - 10, -10)
+      this.x = x;
+      break;
+    case 'up':
+      y = Math.max(this.y - 10, -10)
+      this.y = y;
+      break;
+    case 'right':
+      x = Math.min(this.x + 10, 410);
+      this.x = x;
+      break;
+    case 'down':
+      y = Math.min(this.y + 10, 430);
+      this.y = y;
+      break;
+  }
+}
+
+Player.prototype.update = function() {
+  // do stuff here.
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
